6e7c0037e79d924100a7c18f677d37d678176396
quackingplums <quackingplums@hotmail.com>
2013-01-28 22:45:36 +0000

    Parameterised the thresholds Parameterised the colours

--- create_test_graphs.sh
+++ create_test_graphs.sh
@@ -1,3 +1,5 @@
+. rrd.conf
+
 rrdtool graph test.png \
 	--start N-180000 \
 	DEF:temp=RasPi-TempLogger.rrd:temp:AVERAGE \
@@ -6,8 +8,8 @@ rrdtool graph test.png \
 rrdtool graph test2.png \
 	--start N-180000 \
 	DEF:temp=RasPi-TempLogger.rrd:temp:AVERAGE \
-	CDEF:ok=temp,24,MIN \
-	CDEF:toocool=temp,17,MIN \
+	CDEF:ok=temp,$UPPERTHRESHOLD,MIN \
+	CDEF:toocool=temp,$LOWERTHRESHOLD,MIN \
 	AREA:temp#FF0000:"Too warm" \
 	AREA:ok#00FF00:"Ok" \
 	AREA:toocool#0000FF:"Too cool"
@@ -15,8 +17,8 @@ rrdtool graph test2.png \
 rrdtool graph test3.png \
 	--start N-180000 \
 	DEF:temp=RasPi-TempLogger.rrd:temp:AVERAGE \
-	CDEF:ok=temp,17,24,LIMIT \
-	CDEF:toocool=temp,0,17,LIMIT \
+	CDEF:ok=temp,$LOWERTHRESHOLD,$UPPERTHRESHOLD,LIMIT \
+	CDEF:toocool=temp,0,$LOWERTHRESHOLD,LIMIT \
 	AREA:temp#FF0000:"Too warm" \
 	AREA:ok#00FF00:"Ok" \
 	AREA:toocool#0000FF:"Too cool"
@@ -24,11 +26,11 @@ rrdtool graph test3.png \
 rrdtool graph test4.png \
 	--start N-180000 \
 	DEF:temp=RasPi-TempLogger.rrd:temp:AVERAGE \
-	CDEF:nocolour=temp,17,LE,temp,17,IF \
-	CDEF:toocool=temp,17,LE,17,temp,-,UNKN,IF \
-	CDEF:justright=temp,17,GT,temp,24,GT,7,temp,17,-,IF,UNKN,IF \
-	CDEF:toowarm=temp,24,GT,temp,24,-,UNKN,IF \
+	CDEF:nocolour=temp,$LOWERTHRESHOLD,LE,temp,$LOWERTHRESHOLD,IF \
+	CDEF:toocool=temp,$LOWERTHRESHOLD,LE,$LOWERTHRESHOLD,temp,-,UNKN,IF \
+	CDEF:justright=temp,$LOWERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,GT,$UPPERTHRESHOLD,$LOWERTHRESHOLD,-,temp,$LOWERTHRESHOLD,-,IF,UNKN,IF \
+	CDEF:toowarm=temp,$UPPERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,-,UNKN,IF \
 	AREA:nocolour#FFFFFFAA:: \
-	AREA:toocool#0000FFAA:"Too cool":STACK \
-	AREA:justright#00FF00AA:"Ok":STACK \
-	AREA:toowarm#FF0000AA:"Too warm":STACK
+	AREA:toocool$TOOCOOLCOLOUR:"Too cool":STACK \
+	AREA:justright$JUSTRIGHTCOLOUR:"Ok":STACK \
+	AREA:toowarm$TOOWARMCOLOUR:"Too warm":STACK
