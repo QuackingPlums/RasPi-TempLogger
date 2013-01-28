5e7a3f42c881051f3aeb9e97d9bc752d77347d58
QuackingPlums <QuackingPlums@hotmail.com>
2013-01-28 20:31:25 +0000

    Added .rrd and .png to .gitignore Committed changes to scripts that were missed off last night's push

--- create_db.sh
+++ create_db.sh
@@ -1,8 +1,8 @@
 rrdtool create RasPi-TempLogger.rrd	\
 	--start N-31536000		\
-	--step 600			\
-	DS:temp:GAUGE:1200:U:U		\
-	RRA:AVERAGE:0.5:1:12		\
+	--step 300			\
+	DS:temp:GAUGE:900:U:U		\
+	RRA:AVERAGE:0.9:1:12		\
 	RRA:AVERAGE:0.9:1:288		\
 	RRA:AVERAGE:0.9:12:168		\
 	RRA:AVERAGE:0.9:12:720		\
