3709310c3709f514674539bc3239a4473efe8a82
quackingplums <quackingplums@hotmail.com>
2013-01-30 23:41:18 +0000

    Added datasource for outdoor temperatures to RRD Extended test data to 7 days Refined meteo graph style Updated graphing script to include outdoor temps

--- create_db.sh
+++ create_db.sh
@@ -2,6 +2,7 @@ rrdtool create RasPi-TempLogger.rrd	\
 	--start N-31536000	\
 	--step 300	\
 	DS:temp:GAUGE:600:U:U	\
+	DS:outtemp:GAUGE:600:U:U \
 	RRA:AVERAGE:0.5:1:12	\
 	RRA:AVERAGE:0.5:1:288	\
 	RRA:AVERAGE:0.5:12:168	\
--- create_graphs.sh
+++ create_graphs.sh
@@ -8,6 +8,7 @@ rrdtool graph hourly.png \
 	-c "SHADEB$RRD_SHADEB" \
 	-c "FONT$RRD_FONT" \
 	-c "CANVAS$RRD_CANVAS" \
+	-c "AXIS$RRD_AXIS" \
 	-c "GRID$RRD_GRID" \
 	-c "MGRID$RRD_MGRID" \
 	-c "FRAME$RRD_FRAME" \
@@ -21,6 +22,111 @@ rrdtool graph hourly.png \
 	AREA:toocool$TOOCOOLCOLOUR:"Too cool":STACK \
 	AREA:justright$JUSTRIGHTCOLOUR:"Ok":STACK \
 	AREA:toowarm$TOOWARMCOLOUR:"Too warm":STACK \
-	LINE1:temp$INDOORTEMP:"Indoor temperature [deg C]"
-	
-	
+	LINE1:temp$INDOORTEMP:"Indoor temperature [deg C]" \
+	DEF:outtemp=RasPi-TempLogger.rrd:outtemp:AVERAGE \
+	LINE1:outtemp$OUTDOORTEMP:"Outdoor temperature [deg C]"
+
+# Daily
+rrdtool graph daily.png \
+	--start -1d \
+	-c "BACK$RRD_BACK" \
+	-c "SHADEA$RRD_SHADEA" \
+	-c "SHADEB$RRD_SHADEB" \
+	-c "FONT$RRD_FONT" \
+	-c "CANVAS$RRD_CANVAS" \
+	-c "AXIS$RRD_AXIS" \
+	-c "GRID$RRD_GRID" \
+	-c "MGRID$RRD_MGRID" \
+	-c "FRAME$RRD_FRAME" \
+	-c "ARROW$RRD_ARROW" \
+	DEF:temp=RasPi-TempLogger.rrd:temp:AVERAGE \
+	CDEF:nocolour=temp,$LOWERTHRESHOLD,LE,temp,$LOWERTHRESHOLD,IF \
+	CDEF:toocool=temp,$LOWERTHRESHOLD,LE,$LOWERTHRESHOLD,temp,-,UNKN,IF \
+	CDEF:justright=temp,$LOWERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,GT,$UPPERTHRESHOLD,$LOWERTHRESHOLD,-,temp,$LOWERTHRESHOLD,-,IF,UNKN,IF \
+	CDEF:toowarm=temp,$UPPERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,-,UNKN,IF \
+	AREA:nocolour$NOCOLOUR:: \
+	AREA:toocool$TOOCOOLCOLOUR:"Too cool":STACK \
+	AREA:justright$JUSTRIGHTCOLOUR:"Ok":STACK \
+	AREA:toowarm$TOOWARMCOLOUR:"Too warm":STACK \
+	LINE1:temp$INDOORTEMP:"Indoor temperature [deg C]" \
+	DEF:outtemp=RasPi-TempLogger.rrd:outtemp:AVERAGE \
+	LINE1:outtemp$OUTDOORTEMP:"Outdoor temperature [deg C]"
+
+# Weeky
+rrdtool graph weekly.png \
+	--start -1w \
+	-c "BACK$RRD_BACK" \
+	-c "SHADEA$RRD_SHADEA" \
+	-c "SHADEB$RRD_SHADEB" \
+	-c "FONT$RRD_FONT" \
+	-c "CANVAS$RRD_CANVAS" \
+	-c "AXIS$RRD_AXIS" \
+	-c "GRID$RRD_GRID" \
+	-c "MGRID$RRD_MGRID" \
+	-c "FRAME$RRD_FRAME" \
+	-c "ARROW$RRD_ARROW" \
+	DEF:temp=RasPi-TempLogger.rrd:temp:AVERAGE \
+	CDEF:nocolour=temp,$LOWERTHRESHOLD,LE,temp,$LOWERTHRESHOLD,IF \
+	CDEF:toocool=temp,$LOWERTHRESHOLD,LE,$LOWERTHRESHOLD,temp,-,UNKN,IF \
+	CDEF:justright=temp,$LOWERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,GT,$UPPERTHRESHOLD,$LOWERTHRESHOLD,-,temp,$LOWERTHRESHOLD,-,IF,UNKN,IF \
+	CDEF:toowarm=temp,$UPPERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,-,UNKN,IF \
+	AREA:nocolour$NOCOLOUR:: \
+	AREA:toocool$TOOCOOLCOLOUR:"Too cool":STACK \
+	AREA:justright$JUSTRIGHTCOLOUR:"Ok":STACK \
+	AREA:toowarm$TOOWARMCOLOUR:"Too warm":STACK \
+	LINE1:temp$INDOORTEMP:"Indoor temperature [deg C]" \
+	DEF:outtemp=RasPi-TempLogger.rrd:outtemp:AVERAGE \
+	LINE1:outtemp$OUTDOORTEMP:"Outdoor temperature [deg C]"
+
+# Monthly
+rrdtool graph monthly.png \
+	--start -1m \
+	-c "BACK$RRD_BACK" \
+	-c "SHADEA$RRD_SHADEA" \
+	-c "SHADEB$RRD_SHADEB" \
+	-c "FONT$RRD_FONT" \
+	-c "CANVAS$RRD_CANVAS" \
+	-c "AXIS$RRD_AXIS" \
+	-c "GRID$RRD_GRID" \
+	-c "MGRID$RRD_MGRID" \
+	-c "FRAME$RRD_FRAME" \
+	-c "ARROW$RRD_ARROW" \
+	DEF:temp=RasPi-TempLogger.rrd:temp:AVERAGE \
+	CDEF:nocolour=temp,$LOWERTHRESHOLD,LE,temp,$LOWERTHRESHOLD,IF \
+	CDEF:toocool=temp,$LOWERTHRESHOLD,LE,$LOWERTHRESHOLD,temp,-,UNKN,IF \
+	CDEF:justright=temp,$LOWERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,GT,$UPPERTHRESHOLD,$LOWERTHRESHOLD,-,temp,$LOWERTHRESHOLD,-,IF,UNKN,IF \
+	CDEF:toowarm=temp,$UPPERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,-,UNKN,IF \
+	AREA:nocolour$NOCOLOUR:: \
+	AREA:toocool$TOOCOOLCOLOUR:"Too cool":STACK \
+	AREA:justright$JUSTRIGHTCOLOUR:"Ok":STACK \
+	AREA:toowarm$TOOWARMCOLOUR:"Too warm":STACK \
+	LINE1:temp$INDOORTEMP:"Indoor temperature [deg C]" \
+	DEF:outtemp=RasPi-TempLogger.rrd:outtemp:AVERAGE \
+	LINE1:outtemp$OUTDOORTEMP:"Outdoor temperature [deg C]"
+
+# Yearly
+rrdtool graph yearly.png \
+	--start -1y \
+	-c "BACK$RRD_BACK" \
+	-c "SHADEA$RRD_SHADEA" \
+	-c "SHADEB$RRD_SHADEB" \
+	-c "FONT$RRD_FONT" \
+	-c "CANVAS$RRD_CANVAS" \
+	-c "AXIS$RRD_AXIS" \
+	-c "GRID$RRD_GRID" \
+	-c "MGRID$RRD_MGRID" \
+	-c "FRAME$RRD_FRAME" \
+	-c "ARROW$RRD_ARROW" \
+	DEF:temp=RasPi-TempLogger.rrd:temp:AVERAGE \
+	CDEF:nocolour=temp,$LOWERTHRESHOLD,LE,temp,$LOWERTHRESHOLD,IF \
+	CDEF:toocool=temp,$LOWERTHRESHOLD,LE,$LOWERTHRESHOLD,temp,-,UNKN,IF \
+	CDEF:justright=temp,$LOWERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,GT,$UPPERTHRESHOLD,$LOWERTHRESHOLD,-,temp,$LOWERTHRESHOLD,-,IF,UNKN,IF \
+	CDEF:toowarm=temp,$UPPERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,-,UNKN,IF \
+	AREA:nocolour$NOCOLOUR:: \
+	AREA:toocool$TOOCOOLCOLOUR:"Too cool":STACK \
+	AREA:justright$JUSTRIGHTCOLOUR:"Ok":STACK \
+	AREA:toowarm$TOOWARMCOLOUR:"Too warm":STACK \
+	LINE1:temp$INDOORTEMP:"Indoor temperature [deg C]" \
+	DEF:outtemp=RasPi-TempLogger.rrd:outtemp:AVERAGE \
+	LINE1:outtemp$OUTDOORTEMP:"Outdoor temperature [deg C]"
+
