1ee08014cc865d88658705dc0bd2f12551091a88
QuackingPlums <QuackingPlums@hotmail.com>
2013-02-01 18:52:54 +0000

    New graph styles defined that work well for 8 hours upwards. Need to consider if 60 minutes is worthwhile.

--- create_db.sh
+++ create_db.sh
@@ -5,12 +5,16 @@ rrdtool create RasPi-TempLogger.rrd	\
 	DS:outtemp:GAUGE:600:U:U \
 	RRA:AVERAGE:0.5:1:288	\
 	RRA:AVERAGE:0.5:12:168	\
-	RRA:AVERAGE:0.5:48:180	\
-	RRA:AVERAGE:0.5:144:360	\
-	RRA:AVERAGE:0.5:288:365	\
-	RRA:MAX:0.5:144:14
+	RRA:AVERAGE:0.5:48:186	\
+	RRA:AVERAGE:0.5:144:366	\
+	RRA:AVERAGE:0.5:288:366	\
+	RRA:MAX:0.5:12:168 RRA:MIN:0.5:12:168 \
+	RRA:MAX:0.5:48:186 RRA:MIN:0.5:48:186 \
+	RRA:MAX:0.5:144:366 RRA:MIN:0.5:144:366 \
+	RRA:MAX:0.5:288:366 RRA:MIN:0.5:288:366
 
-# Do I even need these? They're the same resolution as existing archives, right?
-# RRA:AVERAGE:0.5:1:12	\
-# RRA:AVERAGE:0.5:1:96	\
-# RRA:AVERAGE:0.5:144:180	\
+# RRA:CF:xff:1:288	every (5 minute) sample up to 1d
+# RRA:CF:xff:12:168	hourly consolidation up to 7d
+# RRA:CF:xff:48:186	4-hourly consolidation up to 31d (~1m)
+# RRA:CF:xff:144:366	12-hourly consolidation up to 183d (~6m)
+# RRA:CF:xff:288:366	daily consolidation up to 365d (1y)
--- create_graphs.sh
+++ create_graphs.sh
@@ -1,62 +1,14 @@
 . rrd.conf
 
-# testing max, gradient
-rrdtool graph 7dtest.png \
-	--start -7d \
-	--title "7 days" \
-	--vertical-label "degrees C" \
-	-c "BACK$RRD_BACK" \
-	-c "SHADEA$RRD_SHADEA" \
-	-c "SHADEB$RRD_SHADEB" \
-	-c "FONT$RRD_FONT" \
-	-c "CANVAS$RRD_CANVAS" \
-	-c "AXIS$RRD_AXIS" \
-	-c "GRID$RRD_GRID" \
-	-c "MGRID$RRD_MGRID" \
-	-c "FRAME$RRD_FRAME" \
-	-c "ARROW$RRD_ARROW" \
-	DEF:temp=RasPi-TempLogger.rrd:temp:AVERAGE \
-	"CDEF:shading25=temp,0.025,*" "AREA:shading25#C6E6E600::STACK" \
-	"CDEF:shading30=temp,0.025,*" "AREA:shading30#C6E6E611::STACK" \
-	"CDEF:shading35=temp,0.025,*" "AREA:shading35#C6E6E622::STACK" \
-	"CDEF:shading40=temp,0.025,*" "AREA:shading40#C6E6E633::STACK" \
-	"CDEF:shading45=temp,0.025,*" "AREA:shading45#C6E6E644::STACK" \
-	"CDEF:shading50=temp,0.025,*" "AREA:shading50#C6E6E655::STACK" \
-	"CDEF:shading55=temp,0.025,*" "AREA:shading55#C6E6E666::STACK" \
-	"CDEF:shading60=temp,0.025,*" "AREA:shading60#C6E6E677::STACK" \
-	"CDEF:shading65=temp,0.025,*" "AREA:shading65#C6E6E688::STACK" \
-	"CDEF:shading70=temp,0.025,*" "AREA:shading70#C6E6E699::STACK" \
-	"CDEF:shading75=temp,0.025,*" "AREA:shading75#C6E6E6AA::STACK" \
-	"CDEF:shading80=temp,0.025,*" "AREA:shading80#C6E6E6BB::STACK" \
-	"CDEF:shading85=temp,0.025,*" "AREA:shading85#C6E6E6CC::STACK" \
-	"CDEF:shading90=temp,0.025,*" "AREA:shading90#C6E6E6DD::STACK" \
-	"CDEF:shading95=temp,0.65,*" "AREA:shading95#C6E6E6EE::STACK" \
-	LINE1:temp$INDOORTEMPCOLOUR:"Indoor temperature" \
-	DEF:outtemp=RasPi-TempLogger.rrd:outtemp:AVERAGE \
-	LINE1:outtemp$OUTDOORTEMPCOLOUR:"Outdoor temperature" \
-	DEF:maxtemp=RasPi-TempLogger.rrd:temp:MAX \
-	LINE1:maxtemp#FFCC3C:"12h maximum"
-	
-#"CDEF:shading10=temp,0.90,*" "AREA:shading10#E10000" \
-#"CDEF:shading15=temp,0.85,*" "AREA:shading15#D20000" \
-#"CDEF:shading20=temp,0.80,*" "AREA:shading20#C30000" \
-#"CDEF:shading25=temp,0.75,*" "AREA:shading25#B40000" \
-#"CDEF:shading30=temp,0.70,*" "AREA:shading30#A50000" \
-#"CDEF:shading35=temp,0.65,*" "AREA:shading35#960000" \
-#"CDEF:shading40=temp,0.60,*" "AREA:shading40#870000" \
-#"CDEF:shading45=temp,0.55,*" "AREA:shading45#780000" \
-#"CDEF:shading50=temp,0.50,*" "AREA:shading50#690000" \
-#"CDEF:shading55=temp,0.45,*" "AREA:shading55#5A0000" \
-#"CDEF:shading60=temp,0.40,*" "AREA:shading60#4B0000" \
-#"CDEF:shading65=temp,0.35,*" "AREA:shading65#3C0000" \
-#"CDEF:shading70=temp,0.30,*" "AREA:shading70#2D0000" \
-#"CDEF:shading75=temp,0.25,*" "AREA:shading75#180000" \
-#"CDEF:shading80=temp,0.20,*" "AREA:shading80#0F0000" \
-#"CDEF:shading85=temp,0.15,*" "AREA:shading85#000000"
+rm *.png
 
 # 1 hour
 rrdtool graph 1h.png \
 	--start -1h \
+	--title "Last 60 minutes" \
+	--vertical-label "degrees C" \
+	--watermark "©QuackingPlums January 2013" \
+	--lower-limit 0 \
 	-c "BACK$RRD_BACK" \
 	-c "SHADEA$RRD_SHADEA" \
 	-c "SHADEB$RRD_SHADEB" \
@@ -68,21 +20,28 @@ rrdtool graph 1h.png \
 	-c "FRAME$RRD_FRAME" \
 	-c "ARROW$RRD_ARROW" \
 	DEF:temp=RasPi-TempLogger.rrd:temp:AVERAGE \
-	CDEF:nocolour=temp,$LOWERTHRESHOLD,LE,temp,$LOWERTHRESHOLD,IF \
-	CDEF:toocool=temp,$LOWERTHRESHOLD,LE,$LOWERTHRESHOLD,temp,-,UNKN,IF \
-	CDEF:justright=temp,$LOWERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,GT,$UPPERTHRESHOLD,$LOWERTHRESHOLD,-,temp,$LOWERTHRESHOLD,-,IF,UNKN,IF \
-	CDEF:toowarm=temp,$UPPERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,-,UNKN,IF \
-	AREA:nocolour$NOCOLOUR:: \
-	AREA:toocool$TOOCOOLCOLOUR:"Too cool":STACK \
-	AREA:justright$JUSTRIGHTCOLOUR:"Ok":STACK \
-	AREA:toowarm$TOOWARMCOLOUR:"Too warm":STACK \
-	LINE1:temp$INDOORTEMPCOLOUR:"Indoor temperature [deg C]" \
+	CDEF:shading0=temp,0.125,*  AREA:shading0#C6E6E600::STACK \
+	CDEF:shading1=temp,0.125,*  AREA:shading1#C6E6E620::STACK \
+	CDEF:shading2=temp,0.125,*  AREA:shading2#C6E6E640::STACK \
+	CDEF:shading3=temp,0.125,*  AREA:shading3#C6E6E660::STACK \
+	CDEF:shading4=temp,0.125,*  AREA:shading4#C6E6E680::STACK \
+	CDEF:shading5=temp,0.125,*  AREA:shading5#C6E6E69F::STACK \
+	CDEF:shading6=temp,0.125,*  AREA:shading6#C6E6E6BF::STACK \
+	CDEF:shading7=temp,0.125,*  AREA:shading7#C6E6E6DF::STACK \
+	CDEF:shading8=temp,0.125,*  AREA:shading8#C6E6E6FF::STACK \
+	LINE1:temp$INDOORTEMPCOLOUR:"Indoor temperature" \
 	DEF:outtemp=RasPi-TempLogger.rrd:outtemp:AVERAGE \
-	LINE1:outtemp$OUTDOORTEMPCOLOUR:"Outdoor temperature [deg C]"
+	LINE1:outtemp$OUTDOORTEMPCOLOUR:"Outdoor temperature      " \
+	HRULE:$UPPERTHRESHOLD$UPPERTHRESHOLDCOLOUR:"Upper comfort limit [$UPPERTHRESHOLD""C]" \
+	HRULE:$LOWERTHRESHOLD$LOWERTHRESHOLDCOLOUR:"Lower comfort limit [$LOWERTHRESHOLD""C]" \
 
 # 8 hours
 rrdtool graph 8h.png \
 	--start -8h \
+	--title "Last 8 hours" \
+	--vertical-label "degrees C" \
+	--watermark "©QuackingPlums January 2013" \
+	--lower-limit 0 \
 	-c "BACK$RRD_BACK" \
 	-c "SHADEA$RRD_SHADEA" \
 	-c "SHADEB$RRD_SHADEB" \
@@ -94,21 +53,28 @@ rrdtool graph 8h.png \
 	-c "FRAME$RRD_FRAME" \
 	-c "ARROW$RRD_ARROW" \
 	DEF:temp=RasPi-TempLogger.rrd:temp:AVERAGE \
-	CDEF:nocolour=temp,$LOWERTHRESHOLD,LE,temp,$LOWERTHRESHOLD,IF \
-	CDEF:toocool=temp,$LOWERTHRESHOLD,LE,$LOWERTHRESHOLD,temp,-,UNKN,IF \
-	CDEF:justright=temp,$LOWERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,GT,$UPPERTHRESHOLD,$LOWERTHRESHOLD,-,temp,$LOWERTHRESHOLD,-,IF,UNKN,IF \
-	CDEF:toowarm=temp,$UPPERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,-,UNKN,IF \
-	AREA:nocolour$NOCOLOUR:: \
-	AREA:toocool$TOOCOOLCOLOUR:"Too cool":STACK \
-	AREA:justright$JUSTRIGHTCOLOUR:"Ok":STACK \
-	AREA:toowarm$TOOWARMCOLOUR:"Too warm":STACK \
-	LINE1:temp$INDOORTEMPCOLOUR:"Indoor temperature [deg C]" \
+	CDEF:shading0=temp,0.125,*  AREA:shading0#C6E6E600::STACK \
+	CDEF:shading1=temp,0.125,*  AREA:shading1#C6E6E620::STACK \
+	CDEF:shading2=temp,0.125,*  AREA:shading2#C6E6E640::STACK \
+	CDEF:shading3=temp,0.125,*  AREA:shading3#C6E6E660::STACK \
+	CDEF:shading4=temp,0.125,*  AREA:shading4#C6E6E680::STACK \
+	CDEF:shading5=temp,0.125,*  AREA:shading5#C6E6E69F::STACK \
+	CDEF:shading6=temp,0.125,*  AREA:shading6#C6E6E6BF::STACK \
+	CDEF:shading7=temp,0.125,*  AREA:shading7#C6E6E6DF::STACK \
+	CDEF:shading8=temp,0.125,*  AREA:shading8#C6E6E6FF::STACK \
+	LINE1:temp$INDOORTEMPCOLOUR:"Indoor temperature" \
 	DEF:outtemp=RasPi-TempLogger.rrd:outtemp:AVERAGE \
-	LINE1:outtemp$OUTDOORTEMPCOLOUR:"Outdoor temperature [deg C]"
+	LINE1:outtemp$OUTDOORTEMPCOLOUR:"Outdoor temperature      " \
+	HRULE:$UPPERTHRESHOLD$UPPERTHRESHOLDCOLOUR:"Upper comfort limit [$UPPERTHRESHOLD""C]" \
+	HRULE:$LOWERTHRESHOLD$LOWERTHRESHOLDCOLOUR:"Lower comfort limit [$LOWERTHRESHOLD""C]" \
 
 # 1 day
 rrdtool graph 1d.png \
 	--start -1d \
+	--title "Last 24 hours" \
+	--vertical-label "degrees C" \
+	--watermark "©QuackingPlums January 2013" \
+	--lower-limit 0 \
 	-c "BACK$RRD_BACK" \
 	-c "SHADEA$RRD_SHADEA" \
 	-c "SHADEB$RRD_SHADEB" \
@@ -120,21 +86,27 @@ rrdtool graph 1d.png \
 	-c "FRAME$RRD_FRAME" \
 	-c "ARROW$RRD_ARROW" \
 	DEF:temp=RasPi-TempLogger.rrd:temp:AVERAGE \
-	CDEF:nocolour=temp,$LOWERTHRESHOLD,LE,temp,$LOWERTHRESHOLD,IF \
-	CDEF:toocool=temp,$LOWERTHRESHOLD,LE,$LOWERTHRESHOLD,temp,-,UNKN,IF \
-	CDEF:justright=temp,$LOWERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,GT,$UPPERTHRESHOLD,$LOWERTHRESHOLD,-,temp,$LOWERTHRESHOLD,-,IF,UNKN,IF \
-	CDEF:toowarm=temp,$UPPERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,-,UNKN,IF \
-	AREA:nocolour$NOCOLOUR:: \
-	AREA:toocool$TOOCOOLCOLOUR:"Too cool":STACK \
-	AREA:justright$JUSTRIGHTCOLOUR:"Ok":STACK \
-	AREA:toowarm$TOOWARMCOLOUR:"Too warm":STACK \
-	LINE1:temp$INDOORTEMPCOLOUR:"Indoor temperature [deg C]" \
+	CDEF:shading0=temp,0.125,*  AREA:shading0#C6E6E600::STACK \
+	CDEF:shading1=temp,0.125,*  AREA:shading1#C6E6E620::STACK \
+	CDEF:shading2=temp,0.125,*  AREA:shading2#C6E6E640::STACK \
+	CDEF:shading3=temp,0.125,*  AREA:shading3#C6E6E660::STACK \
+	CDEF:shading4=temp,0.125,*  AREA:shading4#C6E6E680::STACK \
+	CDEF:shading5=temp,0.125,*  AREA:shading5#C6E6E69F::STACK \
+	CDEF:shading6=temp,0.125,*  AREA:shading6#C6E6E6BF::STACK \
+	CDEF:shading7=temp,0.125,*  AREA:shading7#C6E6E6DF::STACK \
+	CDEF:shading8=temp,0.125,*  AREA:shading8#C6E6E6FF::STACK \
+	LINE1:temp$INDOORTEMPCOLOUR:"Indoor temperature" \
 	DEF:outtemp=RasPi-TempLogger.rrd:outtemp:AVERAGE \
-	LINE1:outtemp$OUTDOORTEMPCOLOUR:"Outdoor temperature [deg C]"
+	LINE1:outtemp$OUTDOORTEMPCOLOUR:"Outdoor temperature      " \
+	HRULE:$UPPERTHRESHOLD$UPPERTHRESHOLDCOLOUR:"Upper comfort limit [$UPPERTHRESHOLD""C]" \
+	HRULE:$LOWERTHRESHOLD$LOWERTHRESHOLDCOLOUR:"Lower comfort limit [$LOWERTHRESHOLD""C]" \
 
 # 7 days
 rrdtool graph 7d.png \
 	--start -7d \
+	--title "Last 7 days" \
+	--vertical-label "degrees C" \
+	--watermark "©QuackingPlums January 2013" \
 	-c "BACK$RRD_BACK" \
 	-c "SHADEA$RRD_SHADEA" \
 	-c "SHADEB$RRD_SHADEB" \
@@ -145,22 +117,27 @@ rrdtool graph 7d.png \
 	-c "MGRID$RRD_MGRID" \
 	-c "FRAME$RRD_FRAME" \
 	-c "ARROW$RRD_ARROW" \
+	DEF:maxtemp=RasPi-TempLogger.rrd:temp:MAX \
+	DEF:mintemp=RasPi-TempLogger.rrd:temp:MIN \
+	CDEF:shading0=mintemp  AREA:shading0#C6E6E620::STACK \
+	CDEF:shading1=maxtemp,mintemp,-,0.25,*  AREA:shading1#C6E6E670::STACK \
+	CDEF:shading2=maxtemp,mintemp,-,0.25,*  AREA:shading2#C6E6E6A0::STACK \
+	CDEF:shading3=maxtemp,mintemp,-,0.25,*  AREA:shading3#C6E6E6CF::STACK \
+	CDEF:shading4=maxtemp,mintemp,-,0.25,*  AREA:shading4#C6E6E6FF::STACK \
 	DEF:temp=RasPi-TempLogger.rrd:temp:AVERAGE \
-	CDEF:nocolour=temp,$LOWERTHRESHOLD,LE,temp,$LOWERTHRESHOLD,IF \
-	CDEF:toocool=temp,$LOWERTHRESHOLD,LE,$LOWERTHRESHOLD,temp,-,UNKN,IF \
-	CDEF:justright=temp,$LOWERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,GT,$UPPERTHRESHOLD,$LOWERTHRESHOLD,-,temp,$LOWERTHRESHOLD,-,IF,UNKN,IF \
-	CDEF:toowarm=temp,$UPPERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,-,UNKN,IF \
-	AREA:nocolour$NOCOLOUR:: \
-	AREA:toocool$TOOCOOLCOLOUR:"Too cool":STACK \
-	AREA:justright$JUSTRIGHTCOLOUR:"Ok":STACK \
-	AREA:toowarm$TOOWARMCOLOUR:"Too warm":STACK \
-	LINE1:temp$INDOORTEMPCOLOUR:"Indoor temperature [deg C]" \
+	LINE1:temp$INDOORTEMPCOLOUR:"Indoor temperature" \
 	DEF:outtemp=RasPi-TempLogger.rrd:outtemp:AVERAGE \
-	LINE1:outtemp$OUTDOORTEMPCOLOUR:"Outdoor temperature [deg C]"
+	LINE1:outtemp$OUTDOORTEMPCOLOUR:"Outdoor temperature      " \
+	HRULE:$UPPERTHRESHOLD$UPPERTHRESHOLDCOLOUR:"Upper comfort limit [$UPPERTHRESHOLD""C]" \
+	HRULE:$LOWERTHRESHOLD$LOWERTHRESHOLDCOLOUR:"Lower comfort limit [$LOWERTHRESHOLD""C]" \
 
 # 30 days
 rrdtool graph 30d.png \
 	--start -30d \
+	--title "Last 30 days" \
+	--vertical-label "degrees C" \
+	--watermark "©QuackingPlums January 2013" \
+	--lower-limit 0 \
 	-c "BACK$RRD_BACK" \
 	-c "SHADEA$RRD_SHADEA" \
 	-c "SHADEB$RRD_SHADEB" \
@@ -171,22 +148,27 @@ rrdtool graph 30d.png \
 	-c "MGRID$RRD_MGRID" \
 	-c "FRAME$RRD_FRAME" \
 	-c "ARROW$RRD_ARROW" \
+	DEF:maxtemp=RasPi-TempLogger.rrd:temp:MAX \
+	DEF:mintemp=RasPi-TempLogger.rrd:temp:MIN \
+	CDEF:shading0=mintemp  AREA:shading0#C6E6E620::STACK \
+	CDEF:shading1=maxtemp,mintemp,-,0.25,*  AREA:shading1#C6E6E670::STACK \
+	CDEF:shading2=maxtemp,mintemp,-,0.25,*  AREA:shading2#C6E6E6A0::STACK \
+	CDEF:shading3=maxtemp,mintemp,-,0.25,*  AREA:shading3#C6E6E6CF::STACK \
+	CDEF:shading4=maxtemp,mintemp,-,0.25,*  AREA:shading4#C6E6E6FF::STACK \
 	DEF:temp=RasPi-TempLogger.rrd:temp:AVERAGE \
-	CDEF:nocolour=temp,$LOWERTHRESHOLD,LE,temp,$LOWERTHRESHOLD,IF \
-	CDEF:toocool=temp,$LOWERTHRESHOLD,LE,$LOWERTHRESHOLD,temp,-,UNKN,IF \
-	CDEF:justright=temp,$LOWERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,GT,$UPPERTHRESHOLD,$LOWERTHRESHOLD,-,temp,$LOWERTHRESHOLD,-,IF,UNKN,IF \
-	CDEF:toowarm=temp,$UPPERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,-,UNKN,IF \
-	AREA:nocolour$NOCOLOUR:: \
-	AREA:toocool$TOOCOOLCOLOUR:"Too cool":STACK \
-	AREA:justright$JUSTRIGHTCOLOUR:"Ok":STACK \
-	AREA:toowarm$TOOWARMCOLOUR:"Too warm":STACK \
-	LINE1:temp$INDOORTEMPCOLOUR:"Indoor temperature [deg C]" \
+	LINE1:temp$INDOORTEMPCOLOUR:"Indoor temperature" \
 	DEF:outtemp=RasPi-TempLogger.rrd:outtemp:AVERAGE \
-	LINE1:outtemp$OUTDOORTEMPCOLOUR:"Outdoor temperature [deg C]"
+	LINE1:outtemp$OUTDOORTEMPCOLOUR:"Outdoor temperature      " \
+	HRULE:$UPPERTHRESHOLD$UPPERTHRESHOLDCOLOUR:"Upper comfort limit [$UPPERTHRESHOLD""C]" \
+	HRULE:$LOWERTHRESHOLD$LOWERTHRESHOLDCOLOUR:"Lower comfort limit [$LOWERTHRESHOLD""C]" \
 
 # 90 days
 rrdtool graph 90d.png \
 	--start -90d \
+	--title "Last 90 days" \
+	--vertical-label "degrees C" \
+	--watermark "©QuackingPlums January 2013" \
+	--lower-limit 0 \
 	-c "BACK$RRD_BACK" \
 	-c "SHADEA$RRD_SHADEA" \
 	-c "SHADEB$RRD_SHADEB" \
@@ -197,22 +179,27 @@ rrdtool graph 90d.png \
 	-c "MGRID$RRD_MGRID" \
 	-c "FRAME$RRD_FRAME" \
 	-c "ARROW$RRD_ARROW" \
+	DEF:maxtemp=RasPi-TempLogger.rrd:temp:MAX \
+	DEF:mintemp=RasPi-TempLogger.rrd:temp:MIN \
+	CDEF:shading0=mintemp  AREA:shading0#C6E6E620::STACK \
+	CDEF:shading1=maxtemp,mintemp,-,0.25,*  AREA:shading1#C6E6E670::STACK \
+	CDEF:shading2=maxtemp,mintemp,-,0.25,*  AREA:shading2#C6E6E6A0::STACK \
+	CDEF:shading3=maxtemp,mintemp,-,0.25,*  AREA:shading3#C6E6E6CF::STACK \
+	CDEF:shading4=maxtemp,mintemp,-,0.25,*  AREA:shading4#C6E6E6FF::STACK \
 	DEF:temp=RasPi-TempLogger.rrd:temp:AVERAGE \
-	CDEF:nocolour=temp,$LOWERTHRESHOLD,LE,temp,$LOWERTHRESHOLD,IF \
-	CDEF:toocool=temp,$LOWERTHRESHOLD,LE,$LOWERTHRESHOLD,temp,-,UNKN,IF \
-	CDEF:justright=temp,$LOWERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,GT,$UPPERTHRESHOLD,$LOWERTHRESHOLD,-,temp,$LOWERTHRESHOLD,-,IF,UNKN,IF \
-	CDEF:toowarm=temp,$UPPERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,-,UNKN,IF \
-	AREA:nocolour$NOCOLOUR:: \
-	AREA:toocool$TOOCOOLCOLOUR:"Too cool":STACK \
-	AREA:justright$JUSTRIGHTCOLOUR:"Ok":STACK \
-	AREA:toowarm$TOOWARMCOLOUR:"Too warm":STACK \
-	LINE1:temp$INDOORTEMPCOLOUR:"Indoor temperature [deg C]" \
+	LINE1:temp$INDOORTEMPCOLOUR:"Indoor temperature" \
 	DEF:outtemp=RasPi-TempLogger.rrd:outtemp:AVERAGE \
-	LINE1:outtemp$OUTDOORTEMPCOLOUR:"Outdoor temperature [deg C]"
+	LINE1:outtemp$OUTDOORTEMPCOLOUR:"Outdoor temperature      " \
+	HRULE:$UPPERTHRESHOLD$UPPERTHRESHOLDCOLOUR:"Upper comfort limit [$UPPERTHRESHOLD""C]" \
+	HRULE:$LOWERTHRESHOLD$LOWERTHRESHOLDCOLOUR:"Lower comfort limit [$LOWERTHRESHOLD""C]" \
 
 # 180 days
 rrdtool graph 180d.png \
 	--start -180d \
+	--title "Last 180 days" \
+	--vertical-label "degrees C" \
+	--watermark "©QuackingPlums January 2013" \
+	--lower-limit 0 \
 	-c "BACK$RRD_BACK" \
 	-c "SHADEA$RRD_SHADEA" \
 	-c "SHADEB$RRD_SHADEB" \
@@ -223,22 +210,27 @@ rrdtool graph 180d.png \
 	-c "MGRID$RRD_MGRID" \
 	-c "FRAME$RRD_FRAME" \
 	-c "ARROW$RRD_ARROW" \
+	DEF:maxtemp=RasPi-TempLogger.rrd:temp:MAX \
+	DEF:mintemp=RasPi-TempLogger.rrd:temp:MIN \
+	CDEF:shading0=mintemp  AREA:shading0#C6E6E620::STACK \
+	CDEF:shading1=maxtemp,mintemp,-,0.25,*  AREA:shading1#C6E6E670::STACK \
+	CDEF:shading2=maxtemp,mintemp,-,0.25,*  AREA:shading2#C6E6E6A0::STACK \
+	CDEF:shading3=maxtemp,mintemp,-,0.25,*  AREA:shading3#C6E6E6CF::STACK \
+	CDEF:shading4=maxtemp,mintemp,-,0.25,*  AREA:shading4#C6E6E6FF::STACK \
 	DEF:temp=RasPi-TempLogger.rrd:temp:AVERAGE \
-	CDEF:nocolour=temp,$LOWERTHRESHOLD,LE,temp,$LOWERTHRESHOLD,IF \
-	CDEF:toocool=temp,$LOWERTHRESHOLD,LE,$LOWERTHRESHOLD,temp,-,UNKN,IF \
-	CDEF:justright=temp,$LOWERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,GT,$UPPERTHRESHOLD,$LOWERTHRESHOLD,-,temp,$LOWERTHRESHOLD,-,IF,UNKN,IF \
-	CDEF:toowarm=temp,$UPPERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,-,UNKN,IF \
-	AREA:nocolour$NOCOLOUR:: \
-	AREA:toocool$TOOCOOLCOLOUR:"Too cool":STACK \
-	AREA:justright$JUSTRIGHTCOLOUR:"Ok":STACK \
-	AREA:toowarm$TOOWARMCOLOUR:"Too warm":STACK \
-	LINE1:temp$INDOORTEMPCOLOUR:"Indoor temperature [deg C]" \
+	LINE1:temp$INDOORTEMPCOLOUR:"Indoor temperature" \
 	DEF:outtemp=RasPi-TempLogger.rrd:outtemp:AVERAGE \
-	LINE1:outtemp$OUTDOORTEMPCOLOUR:"Outdoor temperature [deg C]"
+	LINE1:outtemp$OUTDOORTEMPCOLOUR:"Outdoor temperature      " \
+	HRULE:$UPPERTHRESHOLD$UPPERTHRESHOLDCOLOUR:"Upper comfort limit [$UPPERTHRESHOLD""C]" \
+	HRULE:$LOWERTHRESHOLD$LOWERTHRESHOLDCOLOUR:"Lower comfort limit [$LOWERTHRESHOLD""C]" \
 
 # 365 days
 rrdtool graph 365d.png \
 	--start -365d \
+	--title "Last 365 days" \
+	--vertical-label "degrees C" \
+	--watermark "©QuackingPlums January 2013" \
+	--lower-limit 0 \
 	-c "BACK$RRD_BACK" \
 	-c "SHADEA$RRD_SHADEA" \
 	-c "SHADEB$RRD_SHADEB" \
@@ -249,16 +241,17 @@ rrdtool graph 365d.png \
 	-c "MGRID$RRD_MGRID" \
 	-c "FRAME$RRD_FRAME" \
 	-c "ARROW$RRD_ARROW" \
+	DEF:maxtemp=RasPi-TempLogger.rrd:temp:MAX \
+	DEF:mintemp=RasPi-TempLogger.rrd:temp:MIN \
+	CDEF:shading0=mintemp  AREA:shading0#C6E6E620::STACK \
+	CDEF:shading1=maxtemp,mintemp,-,0.25,*  AREA:shading1#C6E6E670::STACK \
+	CDEF:shading2=maxtemp,mintemp,-,0.25,*  AREA:shading2#C6E6E6A0::STACK \
+	CDEF:shading3=maxtemp,mintemp,-,0.25,*  AREA:shading3#C6E6E6CF::STACK \
+	CDEF:shading4=maxtemp,mintemp,-,0.25,*  AREA:shading4#C6E6E6FF::STACK \
 	DEF:temp=RasPi-TempLogger.rrd:temp:AVERAGE \
-	CDEF:nocolour=temp,$LOWERTHRESHOLD,LE,temp,$LOWERTHRESHOLD,IF \
-	CDEF:toocool=temp,$LOWERTHRESHOLD,LE,$LOWERTHRESHOLD,temp,-,UNKN,IF \
-	CDEF:justright=temp,$LOWERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,GT,$UPPERTHRESHOLD,$LOWERTHRESHOLD,-,temp,$LOWERTHRESHOLD,-,IF,UNKN,IF \
-	CDEF:toowarm=temp,$UPPERTHRESHOLD,GT,temp,$UPPERTHRESHOLD,-,UNKN,IF \
-	AREA:nocolour$NOCOLOUR:: \
-	AREA:toocool$TOOCOOLCOLOUR:"Too cool":STACK \
-	AREA:justright$JUSTRIGHTCOLOUR:"Ok":STACK \
-	AREA:toowarm$TOOWARMCOLOUR:"Too warm":STACK \
-	LINE1:temp$INDOORTEMPCOLOUR:"Indoor temperature [deg C]" \
+	LINE1:temp$INDOORTEMPCOLOUR:"Indoor temperature" \
 	DEF:outtemp=RasPi-TempLogger.rrd:outtemp:AVERAGE \
-	LINE1:outtemp$OUTDOORTEMPCOLOUR:"Outdoor temperature [deg C]"
+	LINE1:outtemp$OUTDOORTEMPCOLOUR:"Outdoor temperature      " \
+	HRULE:$UPPERTHRESHOLD$UPPERTHRESHOLDCOLOUR:"Upper comfort limit [$UPPERTHRESHOLD""C]" \
+	HRULE:$LOWERTHRESHOLD$LOWERTHRESHOLDCOLOUR:"Lower comfort limit [$LOWERTHRESHOLD""C]" \
 
